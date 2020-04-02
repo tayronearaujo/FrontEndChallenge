500ee359c49ad1415aaca9347765fa7ef2baee1f
tayronearaujo <tayron_machado@hotmail.com>
2020-04-01 23:13:49 -0300

    add front e busca prontos

---
+++ index.js
@@ -0,0 +1,42 @@
+const listElemet = document.querySelector(".content__main");
+const inputElement = document.querySelector(".navbar__search__input");
+
+function userListRender(userList) {
+    const list  =   `
+        <ul class="content__main__list">
+            ${userList.map(user =>
+            `
+                <li class="content__main__list__row">
+                    <div><img class="content__main__list__row__photo" src="${user.photo}"> </div>
+                    <div class="content__main__list__row__name content__main__list__row__users">${user.name}</div>
+                    <div class="content__main__list__row__users">${user.email}</div>
+                    <div class="content__main__list__row__users">${user.phone}</div>
+                    <div class="content__main__list__row__users">${user.city}</div>
+                    <div class="content__main__list__row__icons">
+                        <i class="fas fa-trash content__main__list__row__icons__itens"></i>   
+                        <i class="fas fa-th content__main__list__row__icons__itens"></i>  
+                        <i class="fas fa-check content__main__list__row__icons__itens"></i>  
+                    </div>
+                </li>                   
+            `
+            ).join('')}
+        </ul>
+    `;
+    listElemet.innerHTML = list;
+}
+
+function search() {
+  inputElement.addEventListener("input", event =>{
+    let value = event.target.value.toLowerCase();
+    userListRender(filterUsersBySearchValue(value));
+  });
+}
+
+function filterUsersBySearchValue(searchValue) {
+  return users.filter(
+    item => item.name.toLowerCase().indexOf(searchValue) !== -1
+  );
+}
+
+userListRender(users);
+search();
---
+++ user.js
@@ -0,0 +1,65 @@
+const users = [
+    {
+      photo: "https://randomuser.me/api/portraits/women/90.jpg",
+      name: "Lorraine",
+      email: "lorraine.beck22@example.com",
+      phone: "(960)-861-1890",
+      city: "Uberlândia-MG"
+    },
+    {
+      photo: "https://randomuser.me/api/portraits/women/44.jpg",
+      name: "Lorem",
+      email: "loremipsum@example.com",
+      phone: "(960)-861-1890",
+      city: "Uberlândia-MG"
+    },
+    {
+      photo: "https://randomuser.me/api/portraits/men/86.jpg",
+      name: "Dolor",
+      email: "dolorsit123@mail.com",
+      phone: "(960)-861-1890",
+      city: "São Paulo - SP"
+    },
+    {
+      photo: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Consectut",
+      email: "consectut123@mail.com",
+      phone: "(960)-861-1890",
+      city: "Uberlândia-MG"
+    },
+    {
+      photo: "https://randomuser.me/api/portraits/men/44.jpg",
+      name: "Elit",
+      email: "elitamed@mail.com",
+      phone: "(960)-861-1890",
+      city: "São Paulo - SP"
+    },
+    {
+      photo: "https://randomuser.me/api/portraits/women/81.jpg",
+      name: "Adipiscinq",
+      email: "adipiscinq@mail.com",
+      phone: "(960)-861-1890",
+      city: "São Paulo - SP"
+    },
+    {
+      photo: "https://randomuser.me/api/portraits/men/25.jpg",
+      name: "Ipsum",
+      email: "ipsumdolor12345@mail.com",
+      phone: "(960)-861-1890",
+      city: "Uberlândia-MG"
+    },
+    {
+      photo: "https://randomuser.me/api/portraits/women/14.jpg",
+      name: "Beck Elit",
+      email: "beckelitsit@mail.com ",
+      phone: "(960)-861-1890",
+      city: "Belo Horizonte-MG"
+    },
+    {
+      photo: "https://randomuser.me/api/portraits/women/22.jpg",
+      name: "Lorem",
+      email: "loremipsum@example.com",
+      phone: "(960)-861-1890",
+      city: "São Paulo - SP"
+    },
+  ];
